14a99d8d9370db41eed83ce105ec4b39519ae53c
lmiku <lmik@users.noreply.github.com>
2022-04-23 21:33:56 +0200

    dark mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,50 +1,83 @@
 import { initializeIcons } from '@fluentui/font-icons-mdl2';
-import { Stack, StackItem } from '@fluentui/react';
+import { Stack, StackItem, ThemeProvider, Toggle } from '@fluentui/react';
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Cookies from 'universal-cookie';
 import { ISettings } from '../../backend/src/models/settings-model';
 import { getCrawlerMeta } from './api/meta';
 import './App.css';
 import BotPage from './components/bot-page';
 import GamePage from './components/game-page';
 import LandingPage from './components/landing-page';
+import { darkTheme, defaultTheme } from './themes';
+
+
 initializeIcons();
+
+const THEME_COOKIE_KEY = "darkMode";
+const cookies = new Cookies();
 
 const App: React.FC = () => {
   let [meta, setMeta] = useState<ISettings>({ lastFullRun: (new Date(0)).toISOString(), lastRun: (new Date(0)).toISOString(), key: "" })
-
+  let [darkMode, setDarkMode] = useState<boolean>(false);
 
   useEffect(() => {
     fetchMeta();
+    console.log("cookie state", cookies.get(THEME_COOKIE_KEY), typeof cookies.get(THEME_COOKIE_KEY));
+    if (cookies.get(THEME_COOKIE_KEY)) {
+      console.log("true?");
+      const val = cookies.get(THEME_COOKIE_KEY);
+      if (val === "true") {
+        setDarkMode(true);
+      } else {
+        setDarkMode(false);
+      }
+    }
   }, [])
 
   const fetchMeta = (): void => {
     getCrawlerMeta()
       .then(({ data: { settings } }: ISettings | any) => {
-        console.log(settings);
         setMeta(settings)
       })
       .catch((err: Error) => console.log(err))
   }
   return (
-    <main className='App'>
-      <Stack>
-        <StackItem>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<LandingPage />} />
-              <Route path="/bots" element={<BotPage />} />
-              <Route path="/games" element={<GamePage />} />
-            </Routes>
-          </BrowserRouter>
-        </StackItem>
-        <StackItem>
-          last checked: {meta.lastRun}, last full run: {meta.lastFullRun}
-        </StackItem>
-      </Stack>
+    <React.StrictMode>
 
+      <ThemeProvider theme={darkMode ? darkTheme : defaultTheme}>
+        <main className='App'>
+          <Stack>
+            <StackItem>
+              <Toggle
+                onText='dark'
+                offText='light'
+                defaultChecked={cookies.get(THEME_COOKIE_KEY) === "true"}
+                onChange={(event, checked) => {
 
-    </main >
+                  setDarkMode(checked ?? false);
+                  cookies.set(THEME_COOKIE_KEY, checked ?? false);
+                  console.log("setting cookie", checked, cookies.get(THEME_COOKIE_KEY));
+                }} />
+            </StackItem>
+            <StackItem>
+              <BrowserRouter>
+                <Routes>
+                  <Route path="/" element={<LandingPage />} />
+                  <Route path="/bots" element={<BotPage />} />
+                  <Route path="/games" element={<GamePage />} />
+                </Routes>
+              </BrowserRouter>
+            </StackItem>
+            <StackItem>
+              last checked: {meta.lastRun}, last full run: {meta.lastFullRun}
+            </StackItem>
+          </Stack>
+        </main>
+
+      </ThemeProvider>
+    </React.StrictMode>
+
   )
 }
 export default App;
